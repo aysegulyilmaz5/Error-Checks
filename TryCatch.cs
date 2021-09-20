8e5b96864a4dc60642f34c39653d09d36784cab2
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-09-20 03:11:43 +0300

    Error Checks

---
+++ TryCatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Error_Checks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number1 = Convert.ToInt16(textBox1.Text);
+                int number2 = Convert.ToInt16(textBox2.Text);
+                int sum = number1 + number2;
+                MessageBox.Show(sum.ToString());
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Please check your values");
+            }
+        }
+    }
+}
